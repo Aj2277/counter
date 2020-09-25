745313da6aeb821d17c0d75be95299134124d6b9
Aj2277 <ajurakhanov2277@gmail.com>
2020-09-24 22:33:34 -0700

    Updates

---
+++ precache-manifest.c7854d95a32d3548f1585f3f83adffd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94df624abcbcb67b56e6b2ba4a42b693",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "c8849fbe924721551275",
+    "url": "/counter/static/css/main.04629168.chunk.css"
+  },
+  {
+    "revision": "cec5c1325e5519e58820",
+    "url": "/counter/static/js/2.26c5d0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.26c5d0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8849fbe924721551275",
+    "url": "/counter/static/js/main.5fad45ad.chunk.js"
+  },
+  {
+    "revision": "784d33e82c93a030f1f4",
+    "url": "/counter/static/js/runtime-main.e0439584.js"
+  }
+]);
